bf6ff8aaac4d665c4264c06b3f7d747e267e35fa
nsievert1 <nsieve1@students.towson.edu>
2021-02-21 12:17:52 -0500

    Added test for new calculation method

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,16 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+//	public int substract(int a, int b) {
+//		return a - b;
+//	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -10,4 +10,23 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	
+	@Test
+	public void testSubstract() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.substract(2, 1), 1);
+	}
+	
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(2, 2), 4);
+	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 2), 2);
+
+	}
 }
